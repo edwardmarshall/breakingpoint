96c46e0c70ec3f5d81faa47e0fa09a5abe09bdbe
Eric Taylor <eric@daxos.com>
2013-04-09 10:56:49 -0600

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Breakingpoint::Application.config.secret_token = '54c127f4373d6c5de41aa5dd81bbc81f24eed6c8130ade86ff860658f86913829195ab02f7b6dbd93d25bdb4c9ed5e067c92fe92f7e55086493ce5e709c7b6f1'
