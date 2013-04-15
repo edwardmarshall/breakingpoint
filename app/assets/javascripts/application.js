7e1e58afe74bcce0bb3e310902509d430944dc50
Eric Taylor <eric@daxos.com>
2013-04-15 15:59:53 -0600

    Added scroll and fixed header nav, need to adjust sections margin bottom top to work with nav

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,59 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(document).ready(function() {
+
+	$(document).on('click', '#nav a', function(){
+		var target = $(this);
+		var hash = this.hash;
+		var destination = $(hash).offset().top - 44;
+
+		stopAnimatedScroll();
+
+		$('#nav li').removeClass('on');
+		target.parent().addClass('on');
+
+		 $('html, body').stop().animate({ 
+			scrollTop: destination
+		}, 400, function() { window.location.hash = hash; });
+		return false;
+	});
+
+	function stopAnimatedScroll(){
+		if ( $('*:animated').length > 0 ) { $('*:animated').stop(); }
+	}
+	if(window.addEventListener) {
+	    document.addEventListener('DOMMouseScroll', stopAnimatedScroll, false);
+	}
+	document.onmousewheel = stopAnimatedScroll;
+
+	if($('body').hasClass('home')){
+		console.log("has home");
+		var $filter = $('#nav');
+		var $filterSpacer = $('<div />', {
+			"class": "filter-drop-spacer",
+			"height": $filter.outerHeight()
+		});
+	  
+
+		if ($filter.size())
+		{
+			$(window).scroll(function ()
+			{     
+				if (!$filter.hasClass('fix') && $(window).scrollTop() > $filter.offset().top)
+				{
+					$filter.before($filterSpacer);
+					$filter.addClass("fix");
+				}
+				else if ($filter.hasClass('fix')  && $(window).scrollTop() < $filterSpacer.offset().top)
+				{
+					$filter.removeClass("fix");
+					$filterSpacer.remove();
+				}
+			});
+		}
+	}
+
+});
+
