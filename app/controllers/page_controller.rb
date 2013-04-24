103958ddb3fd72147ab05a4db7e4e3ce7a4d316c
Eric Taylor <eric@daxos.com>
2013-04-24 16:54:04 -0600

    We're LIVE

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -17,27 +17,6 @@ class PageController < ApplicationController
 	end
 
 	def vip
-		authenticate_user!
-
-		@body_class = "vip"
-
-		# Meet 'n' Greet
-		event1 = Time.new(2013, 04, 23, 12, 30, 0, "-06:00") # Time in MDT
-
-		# Pre-Show
-		event2 = Time.new(2013, 04, 25, 11, 30, 0, "-06:00") # Time in MDT
-
-		# Breaking Point 2
-		event3 = Time.new(2013, 04, 25, 13, 0, 0, "-06:00") # Time in MDT
-
-		if Time.now >= event3
-			@event = "bp2"
-		elsif Time.now >= event2
-			@event = "pre-show"
-		elsif Time.now >= event1
-			@event = "meet-n-greet"
-		else
-			@event = "default"
-		end
+		redirect_to root_url
 	end
 end
